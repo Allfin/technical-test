d3345f8ef31ba1b77e129ef395e459bf12687145
All Fine Maulinaro <allfinem10@gmail.com>
2025-02-02 06:23:53 +0700

    fix section one and social icon

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -1,9 +1,57 @@
 <script>
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SocialIcon from "./ui/SocialIcon.vue";
 
+gsap.registerPlugin(ScrollTrigger);
 export default {
   components: {
     SocialIcon,
+  },
+  mounted() {
+    // Inisialisasi animasi saat komponen terpasang
+    this.initAnimation();
+  },
+
+  methods: {
+    initAnimation() {
+      // Animasi menggunakan GSAP dan ScrollTrigger
+      gsap.from(".text", {
+        duration: 1,
+        x: 50,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".text", // Element yang memicu animasi
+          start: "top 80%", // Mulai animasi saat 80% dari elemen masuk ke viewport
+          end: "bottom 20%", // Akhiri animasi saat 20% dari elemen keluar dari viewport
+          toggleActions: "play reverse play reverse",
+        },
+      });
+
+      gsap.from(".logo", {
+        duration: 1,
+        y: -50,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".logo", // Element yang memicu animasi
+          start: "top 80%", // Mulai animasi saat 80% dari elemen masuk ke viewport
+          end: "bottom 20%", // Akhiri animasi saat 20% dari elemen keluar dari viewport
+          toggleActions: "play reverse play reverse",
+        },
+      });
+
+      gsap.from(".social", {
+        duration: 1,
+        y: 50,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".social", // Element yang memicu animasi
+          start: "top 100%",
+          end: "bottom 10%",
+          toggleActions: "play reverse play reverse",
+        },
+      });
+    },
   },
 };
 </script>
@@ -34,24 +82,6 @@ export default {
       max-width: 15%;
       height: auto;
       display: block;
-    }
-
-    .social {
-      .social-icons {
-        display: flex;
-        gap: 10px;
-        justify-content: center;
-
-        .icon {
-          min-width: 3%;
-          min-height: 3%;
-          cursor: pointer;
-          transition: transform 0.3s ease;
-          &:hover {
-            transform: scale(0.8);
-          }
-        }
-      }
     }
   }
 }
--- src/components/ui/SocialIcon.vue
+++ src/components/ui/SocialIcon.vue
@@ -11,10 +11,6 @@
 </template>
 
 <script>
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
 export default {
   data() {
     return {
@@ -31,55 +27,23 @@ export default {
       showFirstImage: true,
     };
   },
-  mounted() {
-    // Inisialisasi animasi saat komponen terpasang
-    this.initAnimation();
-  },
-
-  methods: {
-    initAnimation() {
-      // Animasi menggunakan GSAP dan ScrollTrigger
-      gsap.from(".text", {
-        duration: 1,
-        x: 50,
-        opacity: 0,
-        scrollTrigger: {
-          trigger: ".text", // Element yang memicu animasi
-          start: "top 80%", // Mulai animasi saat 80% dari elemen masuk ke viewport
-          end: "bottom 20%", // Akhiri animasi saat 20% dari elemen keluar dari viewport
-          toggleActions: "play reverse play reverse",
-        },
-      });
-
-      gsap.from(".logo", {
-        duration: 1,
-        y: -50,
-        opacity: 0,
-        scrollTrigger: {
-          trigger: ".logo", // Element yang memicu animasi
-          start: "top 80%", // Mulai animasi saat 80% dari elemen masuk ke viewport
-          end: "bottom 20%", // Akhiri animasi saat 20% dari elemen keluar dari viewport
-          toggleActions: "play reverse play reverse",
-        },
-      });
-
-      gsap.from(".social", {
-        duration: 1,
-        y: 50,
-        opacity: 0,
-        scrollTrigger: {
-          trigger: ".social", // Element yang memicu animasi
-          start: "top 100%",
-          end: "bottom 10%",
-          toggleActions: "play reverse play reverse",
-        },
-      });
-    },
-  },
-  beforeUnmount() {
-    ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
-  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.social-icons {
+  .icon {
+    min-width: 3%;
+    min-height: 3%;
+    cursor: pointer;
+    transition: transform 0.3s ease;
+    &:hover {
+      transform: scale(0.8);
+    }
+  }
+
+  img {
+    width: 35px;
+  }
+}
+</style>
